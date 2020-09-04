8f3b1f3420163e637ccbac49d655d7a65b37a9ec
Kishore <sai.kishore2015@gmail.com>
2020-09-04 18:57:42 +0530

    Updates

---
+++ precache-manifest.1d9147d9b8121ffb876761c7d1c87966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6915df905e72446cea251d20ce8667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dedc2994e5f985027aa8",
+    "url": "/static/css/main.56bf45db.chunk.css"
+  },
+  {
+    "revision": "c35f2fc065629d95b9cb",
+    "url": "/static/js/2.88c903f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88c903f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dedc2994e5f985027aa8",
+    "url": "/static/js/main.7ac5fd12.chunk.js"
+  },
+  {
+    "revision": "a0ec4ea3c0c799b81a78",
+    "url": "/static/js/runtime-main.68d84a47.js"
+  }
+]);
